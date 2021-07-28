9d4ab5709f0f2a73db3bb6b4058c67d48cc5d1fc
Chris Chamberlain <chrisbc@artisan.co.nz>
2021-07-28 14:59:03 +1200

    minor updates

--- src/automation/build_manual_index.py
+++ src/automation/build_manual_index.py
@@ -172,16 +172,12 @@ if __name__ == "__main__":
     headers={"x-api-key":API_KEY}
     general_api = ToshiApi(API_URL, S3_URL, None, with_schema_validation=True, headers=headers)
 
-    GID = "R2VuZXJhbFRhc2s6MTg2NkdyZDRY="
-    GID = "R2VuZXJhbFRhc2s6MTk0NTJNS2dN"
-    GID = "R2VuZXJhbFRhc2s6MjAwNU1veHM5"
     GID = "R2VuZXJhbFRhc2s6NzA4Q3RieTg=" #TEST API example
     GID = "R2VuZXJhbFRhc2s6NzIybjVvc0I=" #TEST RUPT SET
     GID = "R2VuZXJhbFRhc2s6NzI2ejQ4SlQ=" #TEST INVERSION
-    GID = "R2VuZXJhbFRhc2s6MjA2N2d5ZnB2"
-    GID = "R2VuZXJhbFRhc2s6MjM3MHlHVW04"
+    GID = "R2VuZXJhbFRhc2s6MjUyMTJjRzV3"
 
-    UPLOAD_FOLDER = "DATA26"
+    UPLOAD_FOLDER = "DATA28"
 
     TUI = "http://simple-toshi-ui.s3-website-ap-southeast-2.amazonaws.com/"
     WORK_FOLDER = "/home/chrisbc/DEV/GNS/opensha-new/AWS_S3_DATA"
@@ -198,7 +194,8 @@ if __name__ == "__main__":
     #info_keys = ['round',]
     #info_keys = ['mfd_equality_weight', 'mfd_inequality_weight','slip_rate_unnormalized_weight' ] # 'round', 'max_inversion_time'
     #info_keys = ['min_fill_ratio',]# 'growth_size_epsilon'] # for ruptget on subduction
-    info_keys = ['round', 'mfd_mag_gt_5', 'mfd_b_value']
+    #info_keys = ['round', 'mfd_mag_gt_5', 'mfd_b_value']
+    info_keys = ['mfd_mag_gt_5_sans', 'mfd_mag_gt_5_tvz']
     #info_keys = []
 
     #Write Section info
