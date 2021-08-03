747b339b308cc2bfa37f6f5838fae7267a76fcd9
Chris Chamberlain <chrisbc@artisan.co.nz>
2021-08-04 10:17:30 +1200

    add run_inversion_hazard with matplotlib plot drawing and gridded hazard to table;

---
+++ src/automation/run_inversion_hazard.py
@@ -0,0 +1,125 @@
+import os
+import pwd
+import itertools
+import stat
+from pathlib import PurePath
+from subprocess import check_call
+from multiprocessing.dummy import Pool
+
+import datetime as dt
+from dateutil.tz import tzutc
+
+from scaling.toshi_api import ToshiApi
+
+from scaling.opensha_task_factory import OpenshaTaskFactory
+from scaling.file_utils import download_files, get_output_file_ids, get_output_file_id
+
+
+import scaling.inversion_hazard_report_task
+
+
+# Set up your local config, from environment variables, with some sone defaults
+from scaling.local_config import (OPENSHA_ROOT, WORK_PATH, OPENSHA_JRE, FATJAR,
+    JVM_HEAP_MAX, JVM_HEAP_START, USE_API, JAVA_THREADS,
+    API_KEY, API_URL, S3_URL, CLUSTER_MODE)
+
+
+def run_tasks(general_task_id, solutions):
+    task_count = 0
+    task_factory = OpenshaTaskFactory(OPENSHA_ROOT, WORK_PATH, scaling.inversion_hazard_report_task,
+        jre_path=OPENSHA_JRE, app_jar_path=FATJAR,
+        task_config_path=WORK_PATH, jvm_heap_max=JVM_HEAP_MAX, jvm_heap_start=JVM_HEAP_START,
+        pbs_script=CLUSTER_MODE)
+
+    for (sid, rupture_set_info) in solutions.items():
+
+        task_count +=1
+
+        #get FM name
+        #fault_model = rupture_set_info['info']['fault_model']
+        fault_model = 'CFM_0_9_SANSTVZ_D90'
+
+        task_arguments = dict(
+            file_id = str(rupture_set_info['id']),
+            file_path = rupture_set_info['filepath'],
+            fault_model = fault_model,
+            )
+        print(task_arguments)
+
+        job_arguments = dict(
+            task_id = task_count,
+            # round = round,
+            java_threads = JAVA_THREADS,
+            java_gateway_port = task_factory.get_next_port(),
+            working_path = str(WORK_PATH),
+            root_folder = OPENSHA_ROOT,
+            general_task_id = general_task_id,
+            use_api = USE_API,
+            )
+
+        #write a config
+        task_factory.write_task_config(task_arguments, job_arguments)
+
+        script = task_factory.get_task_script()
+
+        script_file_path = PurePath(WORK_PATH, f"task_{task_count}.sh")
+        with open(script_file_path, 'w') as f:
+            f.write(script)
+
+        #make file executable
+        st = os.stat(script_file_path)
+        os.chmod(script_file_path, st.st_mode | stat.S_IEXEC)
+
+        yield str(script_file_path)
+        return
+
+if __name__ == "__main__":
+
+    t0 = dt.datetime.utcnow()
+
+    GENERAL_TASK_ID = None
+    # If you wish to override something in the main config, do so here ..
+    WORKER_POOL_SIZE = 1
+    JVM_HEAP_MAX = 42
+    JAVA_THREADS = 12
+    # USE_API = True #to read the ruptset form the API
+
+    #If using API give this task a descriptive setting...
+    TASK_TITLE = "Inversion diags"
+    TASK_DESCRIPTION = """
+    """
+
+    def call_script(script_name):
+        print("call_script with:", script_name)
+        if CLUSTER_MODE:
+            check_call(['qsub', script_name])
+        else:
+            check_call(['bash', script_name])
+
+    headers={"x-api-key":API_KEY}
+    toshi_api = ToshiApi(API_URL, S3_URL, None, with_schema_validation=True, headers=headers)
+
+    pool = Pool(WORKER_POOL_SIZE)
+    for inversion_task_id in ["R2VuZXJhbFRhc2s6NTQ5ZWttekY="]: # R2VuZXJhbFRhc2s6NjMyUzRDZGM="]: #TEST Inversion
+
+        file_generator = get_output_file_ids(toshi_api, inversion_task_id) #
+        #file_generator = get_output_file_id(toshi_api, "RmlsZToxOTA=") #for file by file ID
+
+        solutions = download_files(toshi_api, file_generator, str(WORK_PATH), overwrite=False)
+
+        scripts = []
+        for script_file in run_tasks(GENERAL_TASK_ID, solutions):
+            print('scheduling: ', script_file)
+            scripts.append(script_file)
+
+        print('task count: ', len(scripts))
+        pool.map(call_script, scripts)
+
+
+    print('worker count: ', WORKER_POOL_SIZE)
+    print("GENERAL_TASK_ID:", GENERAL_TASK_ID)
+
+    pool.close()
+    pool.join()
+
+    print("Done! in %s secs" % (dt.datetime.utcnow() - t0).total_seconds())
---
+++ src/automation/scaling/inversion_hazard_report_task.py
@@ -0,0 +1,119 @@
+import argparse
+import json
+import git
+import time
+import datetime as dt
+from pathlib import PurePath, Path
+from py4j.java_gateway import JavaGateway, GatewayParameters
+from src.automation.scaling.toshi_api import ToshiApi
+
+# Set up local config, from environment variables, with some some defaults
+from src.automation.scaling.local_config import (API_KEY, API_URL, S3_URL)
+from src.automation.hazPlot import plotHazardCurve
+
+class BuilderTask():
+    """
+    The python client for a Diagnostics Report
+    """
+    def __init__(self, job_args):
+
+        self.use_api = job_args.get('use_api', False)
+
+        if self.use_api:
+            self._toshi_api = ToshiApi(API_URL, S3_URL, None, with_schema_validation=True, headers={"x-api-key":API_KEY})
+
+        #setup the java gateway binding
+        self._gateway = JavaGateway(gateway_parameters=GatewayParameters(port=job_args['java_gateway_port']))
+        self._hazard_builder = self._gateway.entry_point.getHazardCalculatorBuilder()
+        self._output_folder = PurePath(job_args.get('working_path'))
+
+    def run(self, task_arguments, job_arguments):
+
+        t0 = dt.datetime.utcnow()
+        print(f"Starting Task at {dt.datetime.utcnow().isoformat()}")
+
+        # Run the task....
+        ta, ja = task_arguments, job_arguments
+
+        self._hazard_builder\
+            .setSolutionFile(ta['file_path'])\
+            .setLinear(True)\
+            .setForecastTimespan(float(50))
+
+        calculator = self._hazard_builder.build();
+
+        #gridded
+        gridCalc = self._gateway.entry_point.getGridHazardCalculator(calculator)
+        gridCalc.setRegion("NZ_TEST_GRIDDED");
+        gridCalc.setSpacing(0.5);
+        gridCalc.createGeoJson(0, "/tmp/gridded-hazard.json");
+
+        # List<List<String>> result = gridCalc.getTabularGridHazards();
+        table_rows = []
+        for row in gridCalc.getTabularGridHazards():
+            table_rows.append([x for x in row])
+        # print (table_rows)
+
+        if self.use_api:
+            column_headers = table_rows[0]
+            column_types = ["double" for x in table_rows[0]]
+            result = self._toshi_api.create_table(table_rows[1:], column_headers, column_types,
+                object_id=ta['file_id'] ,
+                table_name="Inversion Solution Gridded Hazard table")
+            mfd_table_id = result['id']
+            print("created table: ", result['id'])
+
+        ####
+        #Hazard plots
+        ####
+
+        #from google/latlon.net
+        locations = dict(
+            WN = ["Wellington", -41.276825, 174.777969], #-41.288889, 174.777222], OAkley
+            AK = ["Auckland", -36.848461, 174.763336],
+            GN = ["Gisborne", -38.662334, 178.017654],
+            CC = ["Christchurch", -43.525650, 172.639847],
+        )
+
+        plot_folder = Path(self._output_folder, ta['file_id'])
+        plot_folder.mkdir(exist_ok=True)
+
+        print(f'location reports')
+        for code in locations.keys():
+
+            point = locations[code][1:3]
+            years = 50
+            group = 'crustal'
+            gmpe = 'ASK2014'
+
+            table = [row[:2] for row in calculator.tabulariseCalc(*point)]
+
+            print(code, table)
+            plotHazardCurve(table,
+                years=years,
+                title=f"opensha: {locations[code][0]} {group} PGA hazard ({years} year)",
+                subtitle=f"{ta['file_id']}",
+                fileName= PurePath(plot_folder, f"{ta['file_id']}_{code}_hazard_plot_{years}yr.png"))
+
+        t1 = dt.datetime.utcnow()
+        print("Report took %s secs" % (t1-t0).total_seconds())
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config")
+    args = parser.parse_args()
+
+    config_file = args.config
+    f= open(config_file, 'r', encoding='utf-8')
+    config = json.load(f)
+
+    # maybe the JVM App is a little slow to get listening
+    time.sleep(2)
+    # Wait for some more time, scaled by taskid to avoid S3 consistency issue
+    #time.sleep(config['job_arguments']['task_id'] )
+
+    # print(config)
+    task = BuilderTask(config['job_arguments'])
+    task.run(**config)
