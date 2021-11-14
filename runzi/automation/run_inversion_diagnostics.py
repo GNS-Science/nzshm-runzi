4b949679115fce2c04be86b312f890e79cb84e5e
benjamineac <ben.albrecht.chamberlain@gmail.com>
2021-11-15 10:14:21 +1300

    removed pbs_script from run_tasks

--- runzi/automation/run_inversion_diagnostics.py
+++ runzi/automation/run_inversion_diagnostics.py
@@ -34,12 +34,11 @@ from .scaling.local_config import (OPENSHA_ROOT, WORK_PATH, OPENSHA_JRE, FATJAR,
 
 def run_tasks(general_task_id, solutions):
     task_count = 0
-    
+
     factory_class = get_factory(CLUSTER_MODE)
     task_factory = factory_class(OPENSHA_ROOT, WORK_PATH, inversion_diags_report_task,
         jre_path=OPENSHA_JRE, app_jar_path=FATJAR,
-        task_config_path=WORK_PATH, jvm_heap_max=JVM_HEAP_MAX, jvm_heap_start=JVM_HEAP_START,
-        pbs_script=CLUSTER_MODE)
+        task_config_path=WORK_PATH, jvm_heap_max=JVM_HEAP_MAX, jvm_heap_start=JVM_HEAP_START)
     
     print(solutions)
 
