2b10c1ea782dfaf35aae6d017fc9f7c08639349a
benjamineac <ben.albrecht.chamberlain@gmail.com>
2021-12-03 09:44:16 +1300

    work path back in os.walk

--- runzi/cli/openquake_hazard.py
+++ runzi/cli/openquake_hazard.py
@@ -7,12 +7,12 @@ from runzi.automation.scaling.local_config import WORK_PATH
 
 def openquake_hazard_query(*args):
     file_list = []
-    for root, dirs, files in os.walk("docker/runzi-openquake/examples"):
+    for root, dirs, files in os.walk(f"{WORK_PATH}/examples"):
         for file in files:
             if(file.endswith(".ini")):
                 file_list.append(os.path.join(root,file))
-    
-    config = inquirer.list_input('Which ini file would you like to use?', choices=file_list)
+    formatted_file_list = [files[22:] for files in file_list]
+    config = inquirer.list_input('Which ini file would you like to use?', choices=formatted_file_list)
     confirm = inquirer.confirm(f'Are you sure you would like to run hazard for {config}')
     if confirm == True:
         subprocess.run([f'oq engine --run {config}'], shell=True)
@@ -20,5 +20,5 @@ def openquake_hazard_query(*args):
         return
     export = inquirer.confirm('Would you like to export your hazard?')
     if export == True:
-        subprocess.run([f'oq engine --export-outputs 1 /{WORK_PATH}/output',], shell=True)
+        subprocess.run([f'oq engine --export-outputs 1 {WORK_PATH}/output',], shell=True)
     return
