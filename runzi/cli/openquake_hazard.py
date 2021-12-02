38f2a1714ea3ac7969d9f2751d22eb446a44abc3
benjamineac <ben.albrecht.chamberlain@gmail.com>
2021-12-02 17:19:34 +1300

    subprocess shell=True

--- runzi/cli/openquake_hazard.py
+++ runzi/cli/openquake_hazard.py
@@ -9,18 +9,17 @@ def openquake_hazard_query(*args):
     file_list = []
     for root, dirs, files in os.walk("docker/runzi-openquake/examples"):
         for file in files:
-            print(file)
             if(file.endswith(".ini")):
                 file_list.append(os.path.join(root,file))
     
     config = inquirer.list_input('Which ini file would you like to use?', choices=file_list)
     confirm = inquirer.confirm(f'Are you sure you would like to run hazard for {config}')
     if confirm == True:
-        subprocess.run(['oq engine', '--run', config])
+        subprocess.run(['oq engine', '--run', config], shell=True)
     else:
         return
     export = inquirer.confirm('Would you like to export your hazard?')
     if export == True:
-        subprocess.run(['oq engine', '--export-outputs', '1', f'/{WORK_PATH}/output',])
+        subprocess.run(['oq engine', '--export-outputs', '1', f'/{WORK_PATH}/output',], shell=True)
         print('Done')
     return
