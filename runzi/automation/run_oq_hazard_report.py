b86947e10d0dcbb534146f4734ad6e0c73cb35fd
chrisdicaprio <christopher.dicaprio@gmail.com>
2022-05-10 16:11:58 +1200

    initial attempt at oq_hazard_report

---
+++ runzi/automation/run_oq_hazard_report.py
@@ -0,0 +1,65 @@
+#!python3
+"""
+This script produces tasks in either AWS, PBS or LOCAL to produce a hazard report from an oq-engine hazard job
+
+"""
+
+import logging
+import datetime as dt
+
+from runzi.automation.scaling.toshi_api import ToshiApi
+
+from runzi.configuration.oq_hazard_report import build_hazard_report_tasks
+from runzi.automation.scaling.schedule_tasks import schedule_tasks
+
+
+# If you wish to override something in the main config, do so here ..
+WORKER_POOL_SIZE = 1
+
+def build_tasks(args, toshi_api):
+    scripts = []
+    for script_file in build_hazard_report_tasks(args, toshi_api):
+        print('scheduling: ', script_file)
+        scripts.append(script_file)
+    return scripts
+
+
+def run(hazard_ids, WORKER_POOL_SIZE):
+
+    t0 = dt.datetime.utcnow()
+
+    logging.basicConfig(level=logging.INFO)
+
+    loglevel = logging.INFO
+    logging.getLogger('py4j.java_gateway').setLevel(loglevel)
+    logging.getLogger('nshm_toshi_client.toshi_client_base').setLevel(loglevel)
+    logging.getLogger('nshm_toshi_client.toshi_file').setLevel(loglevel)
+    logging.getLogger('urllib3').setLevel(loglevel)
+    logging.getLogger('botocore').setLevel(loglevel)
+    logging.getLogger('git.cmd').setLevel(loglevel)
+
+    log = logging.getLogger(__name__)
+
+    headers={"x-api-key":API_KEY}
+    toshi_api = ToshiApi(API_URL, None, None, with_schema_validation=True, headers=headers)
+
+    args = dict(
+        hazard_ids = hazard_ids
+    )
+
+    tasks = build_tasks(args, toshi_api)
+
+    print('worker count: ', WORKER_POOL_SIZE)
+
+    schedule_tasks(tasks,WORKER_POOL_SIZE)
+
+    print("Done! in %s secs" % (dt.datetime.utcnow() - t0).total_seconds())
+
+
+if __name__ == "__main__":
+
+    hazard_ids = [
+        "ABC"
+    ]
+
+    run(hazard_ids, WORKER_POOL_SIZE)
---
+++ runzi/execute/oq_hazard_report_task.py
@@ -0,0 +1,75 @@
+from pathlib import PurePath, Path
+import argparse
+import json
+import urllib
+import time
+import datetime as dt
+
+from nshm_toshi_client.task_relation import TaskRelation
+from oq_hazard_report.report_builder import ReportBuilder
+
+from runzi.util.aws.s3_folder_upload import upload_to_bucket
+
+from runzi.automation.scaling.toshi_api import ToshiApi
+from runzi.automation.scaling.local_config import (WORK_PATH, API_KEY, API_URL, S3_URL, S3_REPORT_BUCKET)
+
+
+class BuilderTask():
+
+    """
+    The python client for creating OpenQuake hazard reports
+    """
+    def __init__(self, job_args):
+
+        self.use_api = job_args.get('use_api', False)
+        self._output_folder = PurePath(WORK_PATH)
+
+        if self.use_api:
+            headers={"x-api-key":API_KEY}
+            self._toshi_api = ToshiApi(API_URL, S3_URL, None, with_schema_validation=True, headers=headers)
+            self._task_relation_api = TaskRelation(API_URL, None, with_schema_validation=True, headers=headers)
+
+    def run(self, task_arguments, job_arguments):
+        # Run the task....
+        t0 = dt.datetime.utcnow()
+
+        ta, ja = task_arguments, job_arguments
+
+        hazard_report_folder = Path(self._output_folder, ta['file_id'], 'hazard_report')
+        hazard_report_folder.mkdir(parents=True, exist_ok=True)
+
+        report_name = f'Hazard Diagnostics: {ta["file_id"]}'
+        
+        report_builder = ReportBuilder()
+        report_builder.setName(report_name)
+        report_builder.setHazardArchive(ta['file_path'])
+        report_builder.setOutputPath(str(hazard_report_folder))
+
+        report_builder.run()
+
+        t1 = dt.datetime.utcnow()
+        print("Report took %s secs" % (t1-t0).total_seconds())
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config")
+    args = parser.parse_args()
+
+    try:
+        # LOCAL and CLUSTER this is a file
+        config_file = args.config
+        f= open(args.config, 'r', encoding='utf-8')
+        config = json.load(f)
+    except:
+        # for AWS this must be a quoted JSON string
+        config = json.loads(urllib.parse.unquote(args.config))
+
+    # Wait for some more time, scaled by taskid to avoid S3 consistency issue
+    time.sleep(config['job_arguments']['task_id'] )
+
+    # print(config)
+    task = BuilderTask(config['job_arguments'])
+    task.run(**config)
+    upload_to_bucket(config['task_arguments']['file_id'], S3_REPORT_BUCKET)
