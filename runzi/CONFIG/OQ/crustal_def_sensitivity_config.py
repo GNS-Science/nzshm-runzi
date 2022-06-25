a97dc25be2ac98af077288c0bc9b8f780593ffb1
Chris Chamberlain <chrisbc@artisan.co.nz>
2022-06-25 13:32:50 +1200

    add recent configs

---
+++ runzi/CONFIG/OQ/crustal_def_sensitivity_config.py
@@ -0,0 +1,162 @@
+
+gt_description = "Crustal Deformation Sensitivity Test"
+
+logic_tree_permutations = [
+    [{
+        "tag": "Geologic", "weight": 1.0,
+        "permute" : [
+
+            {   "group": "Hik",
+                "members" : [
+                    {"tag": "hiktcrp, b0.979, C4, s0.78", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODEwNw==", "bg_id":"RmlsZToxMDY1MjY="},
+                    {"tag": "hiktlck, b0.979, C4, s0.78", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODA5MQ==", "bg_id":"RmlsZToxMDY1MjU="},
+                    {"tag": "hiktcrp, b0.979, C4, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODA5OA==", "bg_id":"RmlsZToxMDY1Mjk="},
+                    {"tag": "hiktlck, b0.979, C4, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODA3OQ==", "bg_id":"RmlsZToxMDY1MzA="},
+                    {"tag": "hiktcrp, b0.979, C4, s1.22", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODEwNA==", "bg_id":"RmlsZToxMDY1Mjc="},
+                    {"tag": "hiktlck, b0.979, C4, s1.22", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODA5Mw==", "bg_id":"RmlsZToxMDY1Mjg="},
+
+                    {"tag": "hiktcrp, b1.101, C4.1, s0.78", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODE5MQ==", "bg_id":"RmlsZToxMDY1MzM="},
+                    {"tag": "hiktlck, b1.101, C4.1, s0.78", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODA5Ng==", "bg_id":"RmlsZToxMDY1MzQ="},
+                    {"tag": "hiktcrp, b1.101, C4.1, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODE5Ng==", "bg_id":"RmlsZToxMDY1NDA="},
+                    {"tag": "hiktlck, b1.101, C4.1, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODEwMw==", "bg_id":"RmlsZToxMDY1Mzk="},
+                    {"tag": "hiktcrp, b1.101, C4.1, s1.22", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODIwMA==", "bg_id":"RmlsZToxMDY1Mzg="},
+                    {"tag": "hiktlck, b1.101, C4.1, s1.22", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODExNA==", "bg_id":"RmlsZToxMDY1Mzc="}
+
+
+                ]
+            },
+
+            {   "group": "PUY",
+                "members" : [
+                    {"tag": "puy, b0.882, C4, s1", "weight": 1.0, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODIzOQ==", "bg_id":"RmlsZToxMDY1NzA="}
+                ]
+            },
+
+            {   "group": "CRU",
+                "members" : [
+                    {"tag": "Cru_geol, b0.849, C4.1, s0.53", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODMzNw==", "bg_id":"RmlsZToxMDY1NDc="},
+                    {"tag": "Cru_geol, b0.849, C4.1, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODMzNA==", "bg_id":"RmlsZToxMDY1NTM="},
+                    {"tag": "Cru_geol, b0.849, C4.1, s1.47", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODM0MA==", "bg_id":"RmlsZToxMDY1NDI="},
+                    {"tag": "Cru_geol, b0.849, C4.1, s1.61", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODMzMg==", "bg_id":"RmlsZToxMDY1NjQ="},
+
+                    {"tag": "Cru_geol, b0.952, C4.2, s0.53", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODMyNA==", "bg_id":"RmlsZToxMDY1NjI="},
+                    {"tag": "Cru_geol, b0.952, C4.2, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODMyMg==", "bg_id":"RmlsZToxMDY1Njk="},
+                    {"tag": "Cru_geol, b0.952, C4.2, s1.47", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODMyNg==", "bg_id":"RmlsZToxMDY1NTE="},
+                    {"tag": "Cru_geol, b0.952, C4.2, s1.61", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODMzMA==", "bg_id":"RmlsZToxMDY1NDk="},
+
+                    {"tag": "Cru_geol, b1.06, C4.3, s0.53", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODM0Ng==", "bg_id":"RmlsZToxMDY1NTc="},
+                    {"tag": "Cru_geol, b1.06, C4.3, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODM0OQ==", "bg_id":"RmlsZToxMDY1NDM="},
+                    {"tag": "Cru_geol, b1.06, C4.3, s1.47", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODM1MA==", "bg_id":"RmlsZToxMDY1NjY="},
+                    {"tag": "Cru_geol, b1.06, C4.3, s1.61", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODM0OA==", "bg_id":"RmlsZToxMDY1NDU="}
+                ]
+            }
+
+
+        ]
+    }],
+
+    [{
+        "tag": "Geodetic", "weight": 1.0,
+        "permute" : [
+
+            {   "group": "Hik",
+                "members" : [
+                    {"tag": "hiktcrp, b0.979, C4, s0.78", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODEwNw==", "bg_id":"RmlsZToxMDY1MjY="},
+                    {"tag": "hiktlck, b0.979, C4, s0.78", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODA5MQ==", "bg_id":"RmlsZToxMDY1MjU="},
+                    {"tag": "hiktcrp, b0.979, C4, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODA5OA==", "bg_id":"RmlsZToxMDY1Mjk="},
+                    {"tag": "hiktlck, b0.979, C4, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODA3OQ==", "bg_id":"RmlsZToxMDY1MzA="},
+                    {"tag": "hiktcrp, b0.979, C4, s1.22", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODEwNA==", "bg_id":"RmlsZToxMDY1Mjc="},
+                    {"tag": "hiktlck, b0.979, C4, s1.22", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODA5Mw==", "bg_id":"RmlsZToxMDY1Mjg="},
+
+                    {"tag": "hiktcrp, b1.101, C4.1, s0.78", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODE5MQ==", "bg_id":"RmlsZToxMDY1MzM="},
+                    {"tag": "hiktlck, b1.101, C4.1, s0.78", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODA5Ng==", "bg_id":"RmlsZToxMDY1MzQ="},
+                    {"tag": "hiktcrp, b1.101, C4.1, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODE5Ng==", "bg_id":"RmlsZToxMDY1NDA="},
+                    {"tag": "hiktlck, b1.101, C4.1, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODEwMw==", "bg_id":"RmlsZToxMDY1Mzk="},
+                    {"tag": "hiktcrp, b1.101, C4.1, s1.22", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODIwMA==", "bg_id":"RmlsZToxMDY1Mzg="},
+                    {"tag": "hiktlck, b1.101, C4.1, s1.22", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODExNA==", "bg_id":"RmlsZToxMDY1Mzc="}
+
+
+                ]
+            },
+
+            {   "group": "PUY",
+                "members" : [
+                    {"tag": "puy, b0.882, C4, s1", "weight": 1.0, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODIzOQ==", "bg_id":"RmlsZToxMDY1NzA="}
+                ]
+            },
+
+            {   "group": "CRU",
+                "members" : [
+                    {"tag": "cru_geod, b0.849, C4.1, s0.53", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODU2Ng==", "bg_id":"RmlsZToxMDY1NDc="},
+                    {"tag": "cru_geod, b0.849, C4.1, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODU2Nw==", "bg_id":"RmlsZToxMDY1NTM="},
+                    {"tag": "cru_geod, b0.849, C4.1, s1.47", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODU2OQ==", "bg_id":"RmlsZToxMDY1NDI="},
+                    {"tag": "cru_geod, b0.849, C4.1, s1.61", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODU3MQ==", "bg_id":"RmlsZToxMDY1NjQ="},
+
+                    {"tag": "cru_geod, b0.952, C4.2, s0.53", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODU3MA==", "bg_id":"RmlsZToxMDY1NjI="},
+                    {"tag": "cru_geod, b0.952, C4.2, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODU3NA==", "bg_id":"RmlsZToxMDY1Njk="},
+                    {"tag": "cru_geod, b0.952, C4.2, s1.47", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODU3Mw==", "bg_id":"RmlsZToxMDY1NTE="},
+                    {"tag": "cru_geod, b0.952, C4.2, s1.61", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODU3NQ==", "bg_id":"RmlsZToxMDY1NDk="},
+
+                    {"tag": "cru_geod, b1.06, C4.3, s0.53", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODUzMg==", "bg_id":"RmlsZToxMDY1NTc="},
+                    {"tag": "cru_geod, b1.06, C4.3, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODUzNA==", "bg_id":"RmlsZToxMDY1NDM="},
+                    {"tag": "cru_geod, b1.06, C4.3, s1.47", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODUzNg==", "bg_id":"RmlsZToxMDY1NjY="},
+                    {"tag": "cru_geod, b1.06, C4.3, s1.61", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODUzMQ==", "bg_id":"RmlsZToxMDY1NDU="}
+                ]
+            }
+
+
+        ]
+    }],
+
+    [{
+        "tag": "Geodetic With Prior", "weight": 1.0,
+        "permute" : [
+
+            {   "group": "Hik",
+                "members" : [
+                    {"tag": "hiktcrp, b0.979, C4, s0.78", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODEwNw==", "bg_id":"RmlsZToxMDY1MjY="},
+                    {"tag": "hiktlck, b0.979, C4, s0.78", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODA5MQ==", "bg_id":"RmlsZToxMDY1MjU="},
+                    {"tag": "hiktcrp, b0.979, C4, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODA5OA==", "bg_id":"RmlsZToxMDY1Mjk="},
+                    {"tag": "hiktlck, b0.979, C4, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODA3OQ==", "bg_id":"RmlsZToxMDY1MzA="},
+                    {"tag": "hiktcrp, b0.979, C4, s1.22", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODEwNA==", "bg_id":"RmlsZToxMDY1Mjc="},
+                    {"tag": "hiktlck, b0.979, C4, s1.22", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODA5Mw==", "bg_id":"RmlsZToxMDY1Mjg="},
+
+                    {"tag": "hiktcrp, b1.101, C4.1, s0.78", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODE5MQ==", "bg_id":"RmlsZToxMDY1MzM="},
+                    {"tag": "hiktlck, b1.101, C4.1, s0.78", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODA5Ng==", "bg_id":"RmlsZToxMDY1MzQ="},
+                    {"tag": "hiktcrp, b1.101, C4.1, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODE5Ng==", "bg_id":"RmlsZToxMDY1NDA="},
+                    {"tag": "hiktlck, b1.101, C4.1, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODEwMw==", "bg_id":"RmlsZToxMDY1Mzk="},
+                    {"tag": "hiktcrp, b1.101, C4.1, s1.22", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODIwMA==", "bg_id":"RmlsZToxMDY1Mzg="},
+                    {"tag": "hiktlck, b1.101, C4.1, s1.22", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODExNA==", "bg_id":"RmlsZToxMDY1Mzc="}
+                ]
+            },
+
+            {   "group": "PUY",
+                "members" : [
+                    {"tag": "puy, b0.882, C4, s1", "weight": 1.0, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODIzOQ==", "bg_id":"RmlsZToxMDY1NzA="}
+                ]
+            },
+
+            {   "group": "CRU",
+                "members" : [
+                    {"tag": "Cru_geod_wp, b0.849, C4.1, s0.53", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODU4Mg==", "bg_id":"RmlsZToxMDY1NDc="},
+                    {"tag": "Cru_geod_wp, b0.849, C4.1, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODU4OQ==", "bg_id":"RmlsZToxMDY1NTM="},
+                    {"tag": "Cru_geod_wp, b0.849, C4.1, s1.47", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODU5Mg==", "bg_id":"RmlsZToxMDY1NDI="},
+                    {"tag": "Cru_geod_wp, b0.849, C4.1, s1.61", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODU5OQ==", "bg_id":"RmlsZToxMDY1NjQ="},
+
+                    {"tag": "Cru_geod_wp, b0.952, C4.2, s0.53", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODU3OQ==", "bg_id":"RmlsZToxMDY1NjI="},
+                    {"tag": "Cru_geod_wp, b0.952, C4.2, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODU3Ng==", "bg_id":"RmlsZToxMDY1Njk="},
+                    {"tag": "Cru_geod_wp, b0.952, C4.2, s1.47", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODU4MA==", "bg_id":"RmlsZToxMDY1NTE="},
+                    {"tag": "Cru_geod_wp, b0.952, C4.2, s1.61", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODU3Nw==", "bg_id":"RmlsZToxMDY1NDk="},
+
+                    {"tag": "Cru_geod_wp, b1.06, C4.3, s0.53", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODYwOA==", "bg_id":"RmlsZToxMDY1NTc="},
+                    {"tag": "Cru_geod_wp, b1.06, C4.3, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODYwNQ==", "bg_id":"RmlsZToxMDY1NDM="},
+                    {"tag": "Cru_geod_wp, b1.06, C4.3, s1.47", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODYwMg==", "bg_id":"RmlsZToxMDY1NjY="},
+                    {"tag": "Cru_geod_wp, b1.06, C4.3, s1.61", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODYwNg==", "bg_id":"RmlsZToxMDY1NDU="}
+                ]
+            }
+
+
+        ]
+    }]
+
+]
---
+++ runzi/CONFIG/OQ/hik_c_sensitivity_config.py
@@ -0,0 +1,138 @@
+
+gt_description = "Hikurangi Scaling C Sensitivity Test",
+
+logic_tree_permutations = [
+
+        [{
+            "tag": "C = 3.9", "weight": 1.0,
+            "permute" : [
+
+                {   "group": "Hik",
+                    "members" : [
+                        {"tag": "hiktcrp, b0.979, C3.9, s0.78", "weight": 0.166666666666667, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODEwOQ==", "bg_id":"RmlsZToxMDY1MjY="},
+                        {"tag": "hiktlck, b0.979, C3.9, s0.78", "weight": 0.166666666666667, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODA3NQ==", "bg_id":"RmlsZToxMDY1MjU="},
+                        {"tag": "hiktcrp, b0.979, C3.9, s1", "weight": 0.166666666666667, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODA5NQ==", "bg_id":"RmlsZToxMDY1Mjk="},
+                        {"tag": "hiktlck, b0.979, C3.9, s1", "weight": 0.166666666666667, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODA4Mw==", "bg_id":"RmlsZToxMDY1MzA="},
+                        {"tag": "hiktcrp, b0.979, C3.9, s1.22", "weight": 0.166666666666667, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODExMA==", "bg_id":"RmlsZToxMDY1Mjc="},
+                        {"tag": "hiktlck, b0.979, C3.9, s1.22", "weight": 0.166666666666667, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODA5MA==", "bg_id":"RmlsZToxMDY1Mjg="}
+                    ]
+                },
+
+                {   "group": "PUY",
+                    "members" : [
+                        {"tag": "puy, b0.882, C4, s1", "weight": 1.0, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODIzOQ==", "bg_id":"RmlsZToxMDY1NzA="}
+                    ]
+                },
+
+                {   "group": "CRU",
+                    "members" : [
+                        {"tag": "Cru_geol, b0.849, C4.1, s0.53", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODMzNw==", "bg_id":"RmlsZToxMDY1NDc="},
+                        {"tag": "Cru_geol, b0.849, C4.1, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODMzNA==", "bg_id":"RmlsZToxMDY1NTM="},
+                        {"tag": "Cru_geol, b0.849, C4.1, s1.47", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODM0MA==", "bg_id":"RmlsZToxMDY1NDI="},
+                        {"tag": "Cru_geol, b0.849, C4.1, s1.61", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODMzMg==", "bg_id":"RmlsZToxMDY1NjQ="},
+
+                        {"tag": "Cru_geol, b0.952, C4.2, s0.53", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODMyNA==", "bg_id":"RmlsZToxMDY1NjI="},
+                        {"tag": "Cru_geol, b0.952, C4.2, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODMyMg==", "bg_id":"RmlsZToxMDY1Njk="},
+                        {"tag": "Cru_geol, b0.952, C4.2, s1.47", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODMyNg==", "bg_id":"RmlsZToxMDY1NTE="},
+                        {"tag": "Cru_geol, b0.952, C4.2, s1.61", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODMzMA==", "bg_id":"RmlsZToxMDY1NDk="},
+
+                        {"tag": "Cru_geol, b1.06, C4.3, s0.53", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODM0Ng==", "bg_id":"RmlsZToxMDY1NTc="},
+                        {"tag": "Cru_geol, b1.06, C4.3, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODM0OQ==", "bg_id":"RmlsZToxMDY1NDM="},
+                        {"tag": "Cru_geol, b1.06, C4.3, s1.47", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODM1MA==", "bg_id":"RmlsZToxMDY1NjY="},
+                        {"tag": "Cru_geol, b1.06, C4.3, s1.61", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODM0OA==", "bg_id":"RmlsZToxMDY1NDU="}
+                    ]
+                }
+
+
+            ]
+        }],
+
+        [{
+            "tag": "C = 4.0", "weight": 1.0,
+            "permute" : [
+
+                {   "group": "Hik",
+                    "members" : [
+                        {"tag": "hiktcrp, b0.979, C4, s0.78", "weight": 0.166666666666667, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODEwNw==", "bg_id":"RmlsZToxMDY1MjY="},
+                        {"tag": "hiktlck, b0.979, C4, s0.78", "weight": 0.166666666666667, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODA5MQ==", "bg_id":"RmlsZToxMDY1MjU="},
+                        {"tag": "hiktcrp, b0.979, C4, s1", "weight": 0.166666666666667, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODA5OA==", "bg_id":"RmlsZToxMDY1Mjk="},
+                        {"tag": "hiktlck, b0.979, C4, s1", "weight": 0.166666666666667, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODA3OQ==", "bg_id":"RmlsZToxMDY1MzA="},
+                        {"tag": "hiktcrp, b0.979, C4, s1.22", "weight": 0.166666666666667, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODEwNA==", "bg_id":"RmlsZToxMDY1Mjc="},
+                        {"tag": "hiktlck, b0.979, C4, s1.22", "weight": 0.166666666666667, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODA5Mw==", "bg_id":"RmlsZToxMDY1Mjg="}
+
+                    ]
+                },
+
+                {   "group": "PUY",
+                    "members" : [
+                        {"tag": "puy, b0.882, C4, s1", "weight": 1.0, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODIzOQ==", "bg_id":"RmlsZToxMDY1NzA="}
+                    ]
+                },
+
+                {   "group": "CRU",
+                    "members" : [
+                        {"tag": "Cru_geol, b0.849, C4.1, s0.53", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODMzNw==", "bg_id":"RmlsZToxMDY1NDc="},
+                        {"tag": "Cru_geol, b0.849, C4.1, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODMzNA==", "bg_id":"RmlsZToxMDY1NTM="},
+                        {"tag": "Cru_geol, b0.849, C4.1, s1.47", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODM0MA==", "bg_id":"RmlsZToxMDY1NDI="},
+                        {"tag": "Cru_geol, b0.849, C4.1, s1.61", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODMzMg==", "bg_id":"RmlsZToxMDY1NjQ="},
+
+                        {"tag": "Cru_geol, b0.952, C4.2, s0.53", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODMyNA==", "bg_id":"RmlsZToxMDY1NjI="},
+                        {"tag": "Cru_geol, b0.952, C4.2, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODMyMg==", "bg_id":"RmlsZToxMDY1Njk="},
+                        {"tag": "Cru_geol, b0.952, C4.2, s1.47", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODMyNg==", "bg_id":"RmlsZToxMDY1NTE="},
+                        {"tag": "Cru_geol, b0.952, C4.2, s1.61", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODMzMA==", "bg_id":"RmlsZToxMDY1NDk="},
+
+                        {"tag": "Cru_geol, b1.06, C4.3, s0.53", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODM0Ng==", "bg_id":"RmlsZToxMDY1NTc="},
+                        {"tag": "Cru_geol, b1.06, C4.3, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODM0OQ==", "bg_id":"RmlsZToxMDY1NDM="},
+                        {"tag": "Cru_geol, b1.06, C4.3, s1.47", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODM1MA==", "bg_id":"RmlsZToxMDY1NjY="},
+                        {"tag": "Cru_geol, b1.06, C4.3, s1.61", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODM0OA==", "bg_id":"RmlsZToxMDY1NDU="}
+                    ]
+                }
+
+
+            ]
+        }],
+
+        [{
+            "tag": "C = 4.1", "weight": 1.0,
+            "permute" : [
+
+                {   "group": "Hik",
+                    "members" : [
+                        {"tag": "hiktcrp, b0.979, C4.1, s0.78", "weight": 0.166666666666667, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODE4OA==", "bg_id":"RmlsZToxMDY1MjY="},
+                        {"tag": "hiktlck, b0.979, C4.1, s0.78", "weight": 0.166666666666667, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODA4NA==", "bg_id":"RmlsZToxMDY1MjU="},
+                        {"tag": "hiktcrp, b0.979, C4.1, s1", "weight": 0.166666666666667, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODE0Mg==", "bg_id":"RmlsZToxMDY1Mjk="},
+                        {"tag": "hiktlck, b0.979, C4.1, s1", "weight": 0.166666666666667, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODA5Mg==", "bg_id":"RmlsZToxMDY1MzA="},
+                        {"tag": "hiktcrp, b0.979, C4.1, s1.22", "weight": 0.166666666666667, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODE3OA==", "bg_id":"RmlsZToxMDY1Mjc="},
+                        {"tag": "hiktlck, b0.979, C4.1, s1.22", "weight": 0.166666666666667, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODA4MA==", "bg_id":"RmlsZToxMDY1Mjg="}
+
+                    ]
+                },
+
+                {   "group": "PUY",
+                    "members" : [
+                        {"tag": "puy, b0.882, C4, s1", "weight": 1.0, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODIzOQ==", "bg_id":"RmlsZToxMDY1NzA="}
+                    ]
+                },
+
+                {   "group": "CRU",
+                    "members" : [
+                        {"tag": "Cru_geol, b0.849, C4.1, s0.53", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODMzNw==", "bg_id":"RmlsZToxMDY1NDc="},
+                        {"tag": "Cru_geol, b0.849, C4.1, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODMzNA==", "bg_id":"RmlsZToxMDY1NTM="},
+                        {"tag": "Cru_geol, b0.849, C4.1, s1.47", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODM0MA==", "bg_id":"RmlsZToxMDY1NDI="},
+                        {"tag": "Cru_geol, b0.849, C4.1, s1.61", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODMzMg==", "bg_id":"RmlsZToxMDY1NjQ="},
+
+                        {"tag": "Cru_geol, b0.952, C4.2, s0.53", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODMyNA==", "bg_id":"RmlsZToxMDY1NjI="},
+                        {"tag": "Cru_geol, b0.952, C4.2, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODMyMg==", "bg_id":"RmlsZToxMDY1Njk="},
+                        {"tag": "Cru_geol, b0.952, C4.2, s1.47", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODMyNg==", "bg_id":"RmlsZToxMDY1NTE="},
+                        {"tag": "Cru_geol, b0.952, C4.2, s1.61", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODMzMA==", "bg_id":"RmlsZToxMDY1NDk="},
+
+                        {"tag": "Cru_geol, b1.06, C4.3, s0.53", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODM0Ng==", "bg_id":"RmlsZToxMDY1NTc="},
+                        {"tag": "Cru_geol, b1.06, C4.3, s1", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODM0OQ==", "bg_id":"RmlsZToxMDY1NDM="},
+                        {"tag": "Cru_geol, b1.06, C4.3, s1.47", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODM1MA==", "bg_id":"RmlsZToxMDY1NjY="},
+                        {"tag": "Cru_geol, b1.06, C4.3, s1.61", "weight": 0.0833333333333333, "inv_id": "SW52ZXJzaW9uU29sdXRpb25Ocm1sOjEwODM0OA==", "bg_id":"RmlsZToxMDY1NDU="}
+                    ]
+                }
+
+            ]
+        }]
+]
