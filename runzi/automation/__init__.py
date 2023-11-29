1714ac34fc6565462d92b5b696fd7864655c7fc3
chrisdicaprio <christopher.dicaprio@gmail.com>
2023-11-29 13:39:34 +1300

    fix circular import

--- runzi/automation/__init__.py
+++
@@ -1,2 +0,0 @@
-from .run_oq_hazard import run_oq_hazard_f
-from .run_oq_disagg import run_oq_disagg_f
--- scripts/cli.py
+++ scripts/cli.py
@@ -1,8 +1,8 @@
 import click
 import toml
 
-import runzi.automation
-
+from runzi.automation.run_oq_disagg import run_oq_disagg_f
+from runzi.automation.run_oq_hazard import run_oq_hazard_f
 
 def load_config(config_filename: str):
     return toml.load(config_filename)
@@ -15,14 +15,14 @@ def rnz():
 @click.argument("config-filename", type=click.Path(exists=True))
 def run_oq_hazard(config_filename):
     config = load_config(config_filename)
-    runzi.automation.run_oq_hazard_f(config)
+    run_oq_hazard_f(config)
 
 
 @rnz.command(name="oq-disagg", help="launch OpenQuake disagg calculation jobs")
 @click.argument("config-filename", type=click.Path(exists=True))
 def run_oq_hazard(config_filename):
     config = load_config(config_filename)
-    runzi.automation.run_oq_disagg_f(config)
+    run_oq_disagg_f(config)
 
 
 if __name__ == "__main__":
