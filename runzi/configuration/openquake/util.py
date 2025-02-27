7c0b07406159317b8898e66928635d9a97f7c4fa
Chris DiCaprio <christopher.dicaprio@gmail.com>
2025-02-28 09:15:31 +1300

    Cleanup unused code (#209)
    * add vulture
    * remove unused code

--- runzi/configuration/openquake/util.py
+++ runzi/configuration/openquake/util.py
@@ -1,5 +1,4 @@
 from enum import Enum
-from typing import Any, Dict, Generator
 
 
 class ComputePlatform(Enum):
@@ -28,38 +27,3 @@ EC2_CONFIGS = {
         job_def="BigLeverOnDemandEC2-JD", job_queue="BigLeverOnDemandEC2-job-queue", mem=120000, cpu=32
     ),
 }
-
-
-def unpack_keys(d: Dict[str, Any]) -> list[str]:
-    keys = []
-    for k1, v in d.items():
-        for k2 in v.keys():
-            keys.append((k1, k2))
-    return keys  # type: ignore
-
-
-def unpack_values(d: Dict[str, Any]) -> Generator[Any, None, None]:
-    for v in d.values():
-        for v2 in v.values():
-            yield v2
-
-
-def update_arguments(dict1, dict2):
-
-    for name, table in dict2.items():
-        if dict2.get(name):
-            for k, v in table.items():
-                dict1[name][k] = v
-        else:
-            dict1[name] = table
-
-    # return dict1
-
-
-def update_oq_args(oq_args, config_scaler, iter_keys, iter_values):
-
-    update_arguments(oq_args, config_scaler)
-    iter_dict = dict()
-    for k, v in zip(iter_keys, iter_values):
-        iter_dict[k[0]] = {k[1]: v}
-    update_arguments(oq_args, iter_dict)
