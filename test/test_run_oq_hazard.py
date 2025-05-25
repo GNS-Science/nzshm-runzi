30880ba6179509f89ffecca64b38b20858bc03a6
voj <o.jurgens@gns.cri.nz>
2025-05-26 10:40:51 +1200

    [226] upload hazard general task files (#231)

--- test/test_run_oq_hazard.py
+++ test/test_run_oq_hazard.py
@@ -1,3 +1,4 @@
+import pytest
 import runzi.automation.openquake.run_oq_hazard as run_oq_hazard_module
 from runzi.automation.openquake.run_oq_hazard import run_oq_hazard
 from runzi.automation.scaling.local_config import EnvMode
@@ -13,22 +14,70 @@ class MockToshiFile:
         pass
 
 
+class MockGeneralTask:
+    def create_task(self, args):
+        pass
+
+
 class MockToshiApi:
 
     def __init__(self, url, s3_url, auth_token, with_schema_validation=True, headers=None):
         self.file = MockToshiFile()
+        self.general_task = MockGeneralTask()
 
 
-# if AWS mode, check that a file ID is added to the task arguments
+# check that file IDs are added to the task arguments
 def test_create_file(mocker, config_dict):
 
     config_dict["site_params"]["locations_file"] = "sites.csv"
     del config_dict["site_params"]["locations"]
 
+    config_dict["hazard_model"]["gmcm_logic_tree"] = "gmcm_small.json"
+    config_dict["hazard_model"]["srm_logic_tree"] = "srm_small.json"
+    config_dict["hazard_model"]["hazard_config"] = "hazard_config.json"
+
     mocked_build_tasks = mocker.patch.object(run_oq_hazard_module, "build_tasks")
+    mocker.patch.object(run_oq_hazard_module, "USE_API", True)
     mocker.patch.object(run_oq_hazard_module, "CLUSTER_MODE", EnvMode.AWS)
     mocker.patch.object(run_oq_hazard_module, "ToshiApi", MockToshiApi)
     mocker.patch.object(run_oq_hazard_module, "schedule_tasks")
 
     run_oq_hazard(config_dict)
     assert mocked_build_tasks.call_args.args[1]["site_params"]["locations_file_id"] == FILE_ID
+    assert mocked_build_tasks.call_args.args[1]["hazard_model"]["gmcm_logic_tree_id"] == FILE_ID
+    assert mocked_build_tasks.call_args.args[1]["hazard_model"]["srm_logic_tree_id"] == FILE_ID
+    assert mocked_build_tasks.call_args.args[1]["hazard_model"]["hazard_config_id"] == FILE_ID
+
+def test_create_some_files(mocker, config_dict):
+
+    config_dict["site_params"]["locations_file"] = "sites.csv"
+    del config_dict["site_params"]["locations"]
+
+    config_dict["hazard_model"]["gmcm_logic_tree"] = "gmcm_small.json"
+
+    mocked_build_tasks = mocker.patch.object(run_oq_hazard_module, "build_tasks")
+    mocker.patch.object(run_oq_hazard_module, "USE_API", True)
+    mocker.patch.object(run_oq_hazard_module, "CLUSTER_MODE", EnvMode.AWS)
+    mocker.patch.object(run_oq_hazard_module, "ToshiApi", MockToshiApi)
+    mocker.patch.object(run_oq_hazard_module, "schedule_tasks")
+
+    run_oq_hazard(config_dict)
+    assert mocked_build_tasks.call_args.args[1]["site_params"]["locations_file_id"] == FILE_ID
+    assert mocked_build_tasks.call_args.args[1]["hazard_model"]["gmcm_logic_tree_id"] == FILE_ID
+    
+
+def test_consistent_setup(mocker, config_dict):
+
+    config_dict["site_params"]["locations_file"] = "sites.csv"
+    del config_dict["site_params"]["locations"]
+
+    mocker.patch.object(run_oq_hazard_module, "build_tasks")
+    mocker.patch.object(run_oq_hazard_module, "USE_API", False)
+    mocker.patch.object(run_oq_hazard_module, "CLUSTER_MODE", EnvMode.AWS)
+    mocker.patch.object(run_oq_hazard_module, "ToshiApi", MockToshiApi)
+    mocker.patch.object(run_oq_hazard_module, "schedule_tasks")
+
+    with pytest.raises(Exception) as excinfo:
+        run_oq_hazard(config_dict)
+    assert "Toshi API must be enabled when cluster mode is AWS" in str(excinfo.value)
+
