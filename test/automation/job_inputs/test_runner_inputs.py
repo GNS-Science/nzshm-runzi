dcfc1c55689da4effe8b656d9d2c89f466a52bb7
chrisdicaprio <christopher.dicaprio@gmail.com>
2025-09-05 10:24:05 +1200

    test input files with fixtures

--- test/automation/job_inputs/test_runner_inputs.py
+++ test/automation/job_inputs/test_runner_inputs.py
@@ -1,43 +1,34 @@
 from pathlib import Path
 
 import pytest
+import tomlkit
 
-from runzi.automation.runner_inputs import AverageSolutionsInput, AzimuthalRuptureSetsInput
+import runzi.automation.runner_inputs as runner_inputs
+
+fixtures_path = Path(__file__).parent.parent.parent / 'fixtures/automation/job_inputs'
+
+def get_dict_from_toml(filepath):
+    toml_str = Path(filepath).read_text()
+    return tomlkit.parse(toml_str).unwrap()
 
 
-def test_input_from_toml():
-    input_filepath = Path(__file__).parent.parent.parent / 'fixtures/automation/job_inputs/average_solutions.toml'
+def test_input_from_toml_io():
+    input_filepath = fixtures_path / 'average_solutions.toml'
     with input_filepath.open() as input_file:
-        job_input = AverageSolutionsInput.from_toml(input_file)
+        job_input = runner_inputs.AverageSolutionsInput.from_toml(input_file)
     assert job_input
 
 
-average_solutions_data = {
-    'title': 'title',
-    'description': 'description',
-    'solution_groups': [['ABC', 'DEF'], ['123', '456']],
-    'worker_pool_size': 5,
-}
-azimuthal_rupture_sets_data = dict(
-    title = 'title',
-    description =  'description',
-    models = ["CFM_0_9_SANSTVZ_2010", "CFM_0_9_SANSTVZ_D90"],
-    strategies = ['UCERF3',],
-    jump_limits = [ 5.0, ],
-    ddw_ratios = [ 0.5, ],
-    min_sub_sects_per_parents = [ 2, ]  ,
-    min_sub_sections_list = [3, 4, 5],
-    max_cumulative_azimuths = [ 560.0, ],
-    thinning_factors = [0.0, 0.1] ,
-    scaling_relations = [ 'TMG_CRU_2017', ],
-    max_sections = 200,
-)
-class_data = [
-    (AverageSolutionsInput, average_solutions_data),
-    (AzimuthalRuptureSetsInput, azimuthal_rupture_sets_data),
+class_filename = [
+    (runner_inputs.AverageSolutionsInput, 'average_solutions.toml'),
+    (runner_inputs.AzimuthalRuptureSetsInput, 'azimuthal_rupture_sets.toml'),
+    (runner_inputs.CoulombRuptureSetsInput, 'coulomb_rupture_sets.toml'),
 ]
-@pytest.mark.parametrize("cls,data", class_data)
-def test_average_solutions_input(cls, data):
-    job_input = cls(**data)
+@pytest.mark.parametrize("cls,filename", class_filename)
+def test_input_class(cls, filename):
+    input_filepath = fixtures_path / filename
+    data = get_dict_from_toml(input_filepath)
+    job_input = cls.from_toml(input_filepath)
+    job_input_asdict = job_input.model_dump()
     for k, v in data.items():
-        assert getattr(job_input, k) == v
+        assert job_input_asdict[k] == v
