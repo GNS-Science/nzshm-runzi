8098b39c069a292116b772e772bd1a7a651ba579
chrisdicaprio <christopher.dicaprio@gmail.com>
2025-09-10 13:38:01 +1200

    no args is help

--- runzi/scripts/runzi_cli.py
+++ runzi/scripts/runzi_cli.py
@@ -6,11 +6,11 @@ import rupture_sets_cli
 import typer
 import utils_cli
 
-app = typer.Typer(help="The NZ NSHM runzi CLI.")
-app.add_typer(inversion_post_process_cli.app, name="ipp", help="post process inversions")
-app.add_typer(rupture_sets_cli.app, name="rupset", help="create rupture sets")
-app.add_typer(reports_cli.app, name="reports", help="create inversion and rupture set reports")
-app.add_typer(utils_cli.app, name="utils", help="utilities")
+app = typer.Typer(help="The NZ NSHM runzi CLI.", no_args_is_help=True)
+app.add_typer(inversion_post_process_cli.app, name="ipp", help="inversion post processing", no_args_is_help=True)
+app.add_typer(rupture_sets_cli.app, name="rupset", help="create rupture sets", no_args_is_help=True)
+app.add_typer(reports_cli.app, name="reports", help="create inversion and rupture set reports", no_args_is_help=True)
+app.add_typer(utils_cli.app, name="utils", help="utilities", no_args_is_help=True)
 
 if __name__ == "__main__":
     app()
