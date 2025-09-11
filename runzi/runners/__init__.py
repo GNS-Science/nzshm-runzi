80801d5ca910dcff86e8721b8ec57c058e9e9338
chrisdicaprio <christopher.dicaprio@gmail.com>
2025-09-11 15:33:22 +1200

    move soon to be depricated runners to scripts

--- runzi/runners/__init__.py
+++ runzi/runners/__init__.py
@@ -2,7 +2,6 @@ from .average_solutions import AverageSolutionsInput, run_average_solutions
 from .coulomb_rupture_sets import CoulombRuptureSetsInput, run_coulomb_rupture_sets
 from .crustal_inversion import run_crustal_inversion
 from .hazard_inputs import DisaggInput, HazardInput
-from .index_disaggs import run_index_disaggs
 from .oq_convert_solution import run_oq_convert_solution
 from .oq_disagg import run_oq_disagg
 from .oq_hazard import run_oq_hazard
