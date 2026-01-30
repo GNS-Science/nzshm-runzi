8a0da6987aafa4ede89a85d4044def77abec81e0
chrisdicaprio <christopher.dicaprio@gmail.com>
2026-01-30 17:18:16 +1300

    add missing file

---
+++ runzi/runners/subduction_inversion.py
@@ -0,0 +1,38 @@
+"""This module provides the runner class for creating Coulomb rupture sets."""
+
+import runzi.execute.subduction_inversion_solution_task as task_module
+from runzi.automation.scaling.toshi_api import ModelType, SubtaskType
+from runzi.execute.arguments import ArgSweeper, TaskLanguage
+
+from .runner import JobRunner
+
+class SubductionInversionJobRunner(JobRunner):
+    """A class to run subduction inversion jobs."""
+    job_name = "Runzi-automation-subduction-inversion"
+    task_language = TaskLanguage.JAVA
+    subtask_type = SubtaskType.INVERSION
+
+    # java_threads is only used for pbs mode, which is not supported anymore.
+    # It should be set to selector_threads * averaging_threads, but this would need to be done task by task if they
+    # are swept args. It would be possible to add some inversion specific code to the build_tasks function or find the
+    # maximum number of threads before hand or find the maximum number of threads that would be needed before hand.
+    java_threads = 16
+    jvm_heap_max = 32
+
+    ecs_max_job_time_min = 60
+    ecs_memory = 30720
+    ecs_vcpu = 4
+    ecs_job_definition = "Fargate-runzi-opensha-JD"
+    ecs_job_queue = "BasicFargate_Q"
+
+    def __init__(self, job_args: ArgSweeper):
+        """Initialize the SubductionInversionJobRunner.
+
+        Args:
+            job_args: input arguments for the jobs including swept args.
+        """
+        super().__init__(job_args, task_module)
+
+    def get_model_type(self) -> ModelType:
+        """Get the model type for subduction inversion jobs."""
+        return ModelType.SUBDUCTION
