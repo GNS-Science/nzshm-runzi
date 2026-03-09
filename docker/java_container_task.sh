4ecca00fe3d250b7e1f50814fedb660ca801daca
chrisdicaprio <christopher.dicaprio@gmail.com>
2026-03-10 11:56:50 +1300

    remove un-needed cd to non-existant dir

--- docker/java_container_task.sh
+++ docker/java_container_task.sh
@@ -4,8 +4,6 @@ export JAVA_CLASSPATH=${NZSHM22_FATJAR}
 export CLASSNAME=nz.cri.gns.NZSHM22.opensha.util.NZSHM22_PythonGateway
 export NZSHM22_APP_PORT=26533
 
-cd /app/nzshm-runzi
-
 /opt/java/bin/java -Xms4G -Xmx${NZSHM22_SCRIPT_JVM_HEAP_MAX}G -XX:ActiveProcessorCount=${NZSHM22_AWS_JAVA_THREADS} -classpath ${JAVA_CLASSPATH} ${CLASSNAME} > ${NZSHM22_SCRIPT_WORK_PATH}/java_app.${NZSHM22_APP_PORT}.log &
 
 # TODO: we can do away with PYTHON_PREP_MODULE
